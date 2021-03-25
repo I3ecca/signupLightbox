542b3c96bcbfaf3411da84f9f4b952eb0ba68c6e
RLant <rebeccalant@gmail.com>
2021-03-25 13:42:01 -0400

    notes on lightbox

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ $(function () {
   // Fade in lightbox after half a second
   $(".lightbox").delay(500).fadeIn(1000);
 
-
+S
 });
 
+//here is one example on how you would use this in the real world
+//You can have a lightbox fadein after a delay and ask for someone's email. 
